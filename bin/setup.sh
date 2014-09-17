47813ca7fe8d5d3381fde07e67e031533a747c6d
constXife <zombie.with.hair@gmail.com>
2014-09-17 07:56:49 +0700

    Add output command.

--- bin/setup.sh
+++ bin/setup.sh
@@ -5,16 +5,19 @@ echo "Installing Elf System"
 SCRIPTPATH=$( cd $(dirname $0) ; cd .. ; pwd -P )
 
 if [ -d "$SCRIPTPATH/vendor" ]; then
-    bundle install --deployment \
+    CMD="bundle install --deployment \
                    --binstubs \
                    --path=$SCRIPTPATH \
                    --gemfile=$SCRIPTPATH/Gemfile \
                    --without=development test \
-                   --local
+                   --local"
 else
-    bundle install --deployment \
+    CMD="bundle install --deployment \
                    --binstubs \
                    --path=$SCRIPTPATH \
                    --gemfile=$SCRIPTPATH/Gemfile \
-                   --without=development test
+                   --without=development test"
 fi
+
+echo $CMD
+$CMD
