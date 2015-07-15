be513141d5556e8cb5c45b98c5a62c8791eb7005
Karim Topoev <zombie.with.hair@gmail.com>
2015-07-15 12:13:17 +0600

    Refactoring.

--- app/assets/javascripts/controllers/sensors/show.js
+++ app/assets/javascripts/controllers/sensors/show.js
@@ -1,7 +1,7 @@
 "use strict";
 
 angular.module('bastet')
-  .controller('SensorsShowCtl', function($scope, $log, $stateParams, $timeout, $window, config, Sensor) {
+  .controller('SensorsShowCtl', function($scope, $stateParams, Sensor) {
       $scope.isLoading = true;
       $scope.sensor = Sensor.show({id: $stateParams.sensor_id, daily: true});
       $scope.sensor.$promise.then(function () {
