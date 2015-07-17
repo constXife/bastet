51565f70941ebbca711ac7533b9641f367a090f4
constXife <zombie.with.hair@gmail.com>
2015-07-18 01:08:46 +0600

    Format.

--- app/assets/javascripts/controllers/dashboard.js
+++ app/assets/javascripts/controllers/dashboard.js
@@ -2,9 +2,9 @@
 
 angular.module('bastet')
   .controller('DashboardCtl', function($scope, $log, Sensor) {
-      $scope.isLoading = true;
-      $scope.sensors = Sensor.index();
-      $scope.sensors.$promise.then(function() {
-        $scope.isLoading = false;
-      });
+    $scope.isLoading = true;
+    $scope.sensors = Sensor.index();
+    $scope.sensors.$promise.then(function() {
+      $scope.isLoading = false;
+    });
   });
