f8ca6bb4fcbe12bec692436c3b3447ec1e45afc9
constXife <zombie.with.hair@gmail.com>
2015-07-17 09:05:50 +0600

    Remove unused vars.

--- app/assets/javascripts/controllers/sensors/show.js
+++ app/assets/javascripts/controllers/sensors/show.js
@@ -1,10 +1,10 @@
 "use strict";
 
 angular.module('bastet')
-  .controller('SensorsShowCtl', function($scope, $log, $stateParams, $timeout, $window, config, Sensor) {
+  .controller('SensorsShowCtl', function($scope, $log, $stateParams, Sensor) {
       $scope.isLoading = true;
       $scope.sensor = Sensor.show({id: $stateParams.sensor_id, daily: true});
       $scope.sensor.$promise.then(function () {
         $scope.isLoading = false;
       });
-    });
+  });
